4ae6dcd021ba6234e2bf6f2069ee173b9f02245c
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-09-11 16:04:57 -0500

    build: add fetch request to Products.jsx

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
+function ProductsList() {
+  // https://fakestoreapi.com/products
+  const [productsList, setProductsList] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => setProductsList(response))
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (productsList === null) {
+    return <h1>Loading...</h1>;
+  }
+  return <h1>Content loaded!</h1>
+}
+
 export default function Products() {
-  return <h1>Products</h1>
+  return <ProductsList />;
 }
