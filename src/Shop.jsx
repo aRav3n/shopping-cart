f2ec651b8186f409120d5d1a6f292aa8a49cd4a5
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-09-11 15:43:54 -0500

    build: nest Products and Cart inside of Shop

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Nav from "./Navbar";
+import styles from "./styles/shop.module.css";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <h1>Shopping</h1>
+      <h1 className={styles.heading}>Shopping</h1>
+      <div className={styles.container}>
+        <Outlet />
+      </div>
     </>
   );
 }
