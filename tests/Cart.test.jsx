03feda30bd3be8c3637b5164493db04f8152f7a6
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-09-16 13:12:09 -0500

    test: write test for <Cart /> component

--- tests/Cart.test.jsx
+++ tests/Cart.test.jsx
@@ -1,12 +1,44 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { MemoryRouter } from "react-router-dom";
+import { vi } from "vitest";
+import { useOutletContext } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 import Cart from "../src/Cart.jsx";
 
-describe("Cart",() => {
-  it("Shows total");
-  it("Shows pay now button");
-  it("Shows info <p> after click of pay button");
-})
+vi.mock("react-router-dom", () => {
+  return {
+    ...vi.importActual("react-router-dom"),
+    useOutletContext: vi.fn(),
+  };
+});
+
+describe("Cart", () => {
+  beforeEach(() => {
+    useOutletContext.mockReturnValue({
+      cart: [],
+    });
+  });
+  it("Shows total", () => {
+    render(<Cart />);
+    const totalText = screen.getByText(/total:/i);
+    expect(totalText).toBeInTheDocument();
+  });
+  it("Shows pay now button", () => {
+    render(<Cart />);
+    const button = screen.getByRole("button", { name: "Pay Now!" });
+    expect(button).toBeInTheDocument();
+  });
+  it("Shows info <p> after button click", async () => {
+    const user = userEvent.setup();
+    render(<Cart />);
+    const button = screen.getByRole("button", { name: "Pay Now!" });
+
+    await user.click(button);
+
+    expect(
+      screen.getByText(/you can't actually buy these items/i)
+    ).toBeInTheDocument();
+  });
+});
---
+++ tests/Shop.test.jsx
@@ -0,0 +1,6 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+
+import Shop from "../src/Shop.jsx";
