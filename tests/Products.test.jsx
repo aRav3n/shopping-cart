9c3da0e5398b7f51068a6d8490b1206e1a5e148e
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-09-16 13:37:51 -0500

    test: write simple test for Products element

--- tests/Products.test.jsx
+++ tests/Products.test.jsx
@@ -1,6 +1,37 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { MemoryRouter } from "react-router-dom";
+import { vi } from "vitest";
+import { useOutletContext } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 import Products from "../src/Products.jsx";
+
+vi.mock("react-router-dom", () => {
+  return {
+    ...vi.importActual("react-router-dom"),
+    useOutletContext: vi.fn(),
+  };
+});
+
+vi.mock("node-fetch", () => ({
+  __esModule: true,
+  default: vi.fn(),
+}));
+
+describe("Product element test", () => {
+  beforeEach(() => {
+    useOutletContext.mockReturnValue({
+      cart: [],
+      setCart: vi.fn(),
+      itemAdded: Boolean,
+    });
+  });
+  it("Shows loading...", () => {
+    render(<Products />);
+    const loadingMessage = screen.getByRole("heading", {
+      name: /loading.../i,
+    });
+    expect(loadingMessage).toBeInTheDocument();
+  });
+});
