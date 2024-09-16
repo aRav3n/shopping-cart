37d444a00e9a961663b48b0c8e9fb21655249226
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-09-16 13:13:02 -0500

    test: get rid of unnecessary import in Cart test

--- tests/Cart.test.jsx
+++ tests/Cart.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
 import { useOutletContext } from "react-router-dom";
